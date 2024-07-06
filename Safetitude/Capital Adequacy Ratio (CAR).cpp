253bca5ac1bfaee9ae43056ac71ab5d43ca9bbaa
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-06 21:32:09 +0530

    Add files via upload

---
+++ Safetitude/Capital Adequacy Ratio (CAR).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double bankCapital, riskWeightedAssets;
+    cout << "Enter bank capital: ";
+    cin >> bankCapital;
+    cout << "Enter risk-weighted assets: ";
+    cin >> riskWeightedAssets;
+
+    double car = (bankCapital / riskWeightedAssets) * 100;
+    cout << "Capital Adequacy Ratio (CAR): " << car << "%" << endl;
+    return 0;
+}
